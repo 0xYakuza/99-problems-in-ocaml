a85ac6551542fec3b1ebbf08bd52ac04122d65c4
AzerothA <telifesite@gmail.com>
2023-05-24 22:12:42 +0330

    problem 02 solved

---
+++ problems/problem02.ml
@@ -0,0 +1,7 @@
+(* Find the last but one (last and penultimate) elements of a list *)
+
+let rec last_two (xs : 'a list) : 'a list option =
+  match xs with
+  | [] -> None
+  | [ x; y ] -> Some [ x; y ]
+  | _ :: last -> last_two last
